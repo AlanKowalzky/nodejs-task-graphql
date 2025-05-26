02b32514a318c84e0a38ad56618010bf84e95532
AlanKowalzky <alan.kowalzky@gmail.com>
2025-05-27 01:39:26 +0200

    feat: b test branch dev_cf2

--- src/routes/graphql/middleware.ts
+++ src/routes/graphql/middleware.ts
@@ -1,10 +1,12 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import depthLimit from 'graphql-depth-limit';
+import { DocumentNode, parse } from 'graphql';
 
 export const depthLimitMiddleware = async (request: FastifyRequest, reply: FastifyReply) => {
   const { query } = request.body as { query: string };
   const depth = depthLimit(5);
-  const errors = depth({ document: query } as any);
+  const document = parse(query);
+  const errors = depth({ document });
   if (errors.length > 0) {
     return reply.code(400).send({ errors });
   }
